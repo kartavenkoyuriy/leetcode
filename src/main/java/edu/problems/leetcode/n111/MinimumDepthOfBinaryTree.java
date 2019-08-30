4e114b1ae37cfec2e41adb80b0c3b403f4bf6091
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2019-08-30 21:16:22 +0300

    n111. solution with concise version

---
+++ src/main/java/edu/problems/leetcode/n111/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,33 @@
+package edu.problems.leetcode.n111;
+
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int minLeft = minDepth(root.left);
+        int minRight = minDepth(root.right);
+//        if (minLeft == 0 && minRight == 0) {
+//            return 1;
+//        }
+//        if (minLeft == 0) {
+//            return minRight + 1;
+//        }
+//
+//        if (minRight == 0) {
+//            return minLeft + 1;
+//        }
+//        return 1 + Math.min(minLeft, minRight);
+        return (minLeft == 0 || minRight == 0) ? minLeft + minRight + 1: Math.min(minLeft,minRight) + 1;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
